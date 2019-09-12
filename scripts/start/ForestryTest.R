e5320c06e1a0bfa7b3a311598563450fcc0d4460
Abhijeet <mishra@pik-potsdam.de>
2019-09-12 17:27:00 +0200

    Test with self suff of potato for wood and livst_rum for woodfuel

--- scripts/start/ForestryTest.R
+++ scripts/start/ForestryTest.R
@@ -9,7 +9,7 @@
 #### Script to MAgPIE test runs ####
 ##########################################################
 
-flag_run <- "cell"
+flag_run <- "ssOther"
 
 library(lucode)
 source("scripts/start_functions.R")
